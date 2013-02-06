a3c73ce31e097565478c6fca8f64cba876a95b51
jpberdel <jp.berdel@gmail.com>
2013-02-06 09:56:08 +0100

    stubs are great

---
+++ YaS4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace YaS4
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ YaS4Core/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace YaS4Core
+{
+    public class Class1
+    {
+    }
+}
